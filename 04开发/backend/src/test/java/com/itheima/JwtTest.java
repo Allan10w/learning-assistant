c242a2c88538bf91c856a455d53c6568435dfdc9
Allan <1484096635@qq.com>
2025-11-24 17:34:05 +0800

    更新JwtTest中的token过期时间为24小时

--- 04开发/backend/src/test/java/com/itheima/JwtTest.java
+++ 04开发/backend/src/test/java/com/itheima/JwtTest.java
@@ -13,14 +13,14 @@ public class JwtTest {
 
     @Test
     public void testGenerateJwt() {
-        //构建 JWT 令牌
+        // 构建 JWT 令牌
         Map<String, Object> dataMap = new HashMap<>();
         dataMap.put("id", 1);
         dataMap.put("username", "admin");
-        String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256, "aXRoZWltYQ==")//指定加密算法和密钥
-                .addClaims(dataMap)//添加自定义属性
-                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))//设置过期时间1小时
-                .compact();//构建令牌
+        String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256, "aXRoZWltYQ==")// 指定加密算法和密钥
+                .addClaims(dataMap)// 添加自定义属性
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))// 设置过期时间1小时
+                .compact();// 构建令牌
         System.out.println(jwt);
     }
 
@@ -29,9 +29,17 @@ public class JwtTest {
      */
     @Test
     public void testParseJwt() {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsImV4cCI6MTc1ODI3MDUxM30.aq5b7Be4HqwYwCpd68texM_B0hyaOwsYICQJPbXll7w";
+        // 生成一个有效期为 24 小时的 JWT
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("id", 1); 
+        dataMap.put("username", "admin");
+        String token = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, "aXRoZWltYQ==")
+                .addClaims(dataMap)
+                .setExpiration(new Date(System.currentTimeMillis() + 24 * 3600 * 1000))
+                .compact();
         Claims claims = Jwts.parser().setSigningKey("aXRoZWltYQ==")
-                .parseClaimsJws(token)//校验
+                .parseClaimsJws(token)
                 .getBody();
         System.out.println(claims);
     }
