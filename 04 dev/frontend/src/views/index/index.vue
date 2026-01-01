34e693ee80e6a81417697bf0d243e3fe6378c12a
allanchanice_admin <1484096635@qq.com>
2026-01-01 13:07:04 +0800

    feat: 彻底更名首页标题、文档名称并同步至 dev 分支

--- 04 dev/frontend/src/views/index/index.vue
+++ 04 dev/frontend/src/views/index/index.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <img src="../../assets/index.png">
+  <div style="text-align: center; padding: 50px;">
+    <h1 style="font-size: 32px; color: #333; margin-bottom: 20px;">欢迎使用校园管理系统</h1>
+    <img src="../../assets/index.png" alt="系统首页图" style="max-width: 100%; height: auto;">
+  </div>
 </template>
 
 <style scoped>
